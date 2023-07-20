69b5d92e5970904ff3ff2bfb3c4ec57ed8ec7193
Mikhail Kardash <mikhail.kardash@hpe.com>
2023-07-20 15:50:59 -0700

    bump some nightly test dependencies

--- examples/computer_vision/efficientdet_pytorch/startup-hook.sh
+++ examples/computer_vision/efficientdet_pytorch/startup-hook.sh
@@ -1,7 +1,7 @@
-pip install timm==0.3.1
+pip install timm==0.9
 # pycocotools 2.0.5, a dependency of efficientdet-pytorch,
 # would not install without cython
-pip install pycocotools==2.0.4
+pip install pycocotools==2.0.6
 git clone https://github.com/rwightman/efficientdet-pytorch.git
 cd efficientdet-pytorch
 git checkout 611532db49fdd691f48f913bc433391a12014bd8
